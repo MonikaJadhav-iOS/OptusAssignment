e98e7fcd19a5fa11c6b2e6aed8808e93fccf526d
Monika Jadhav <thakre.monika54@gmail.com>
2020-12-16 12:01:42 +0530

    Initial code pushed.

---
+++ WeatherInformation/WeatherInformation/AppDelegate/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  WeatherInformation
+//
+//  Created by Monika Jadhav on 16/12/20.
+//  Copyright © 2020 Monika Jadhav. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+    
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+    
+    
+}
---
+++ WeatherInformation/WeatherInformation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherInformation
+//
+//  Created by Monika Jadhav on 16/12/20.
+//  Copyright © 2020 Monika Jadhav. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
