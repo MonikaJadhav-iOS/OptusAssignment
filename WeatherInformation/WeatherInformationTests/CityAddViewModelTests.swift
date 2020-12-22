d3152ddf017750c22975b6fed2b1eb350aa8de22
Monika Jadhav <thakre.monika54@gmail.com>
2020-12-22 10:18:46 +0530

    Added some unit and ui test cases.

---
+++ WeatherInformation/WeatherInformationTests/CityAddViewModelTests.swift
@@ -0,0 +1,66 @@
+//
+//  CityAddViewModelTests.swift
+//  WeatherInformationTests
+//
+//  Created by Monika Jadhav on 21/12/20.
+//  Copyright © 2020 Monika Jadhav. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherInformation
+
+
+class CityAddViewModelTests: XCTestCase {
+    
+    let cityAddViewModel = CityAddViewModel()
+    
+    override func setUp() {
+        if let url = Bundle.main.url(forResource:"currentlist", withExtension: "json") {
+            do {
+                let data = try Data(contentsOf: url)
+                let decoder = JSONDecoder()
+                let jsonData = try decoder.decode([CityInformation].self, from: data)
+                self.cityAddViewModel.cityOriginalArray = jsonData
+                self.cityAddViewModel.cityInformation = self.cityAddViewModel.cityOriginalArray
+            } catch {
+                print("error:\(error)")
+            }
+        }
+    }
+    
+    func testFetchCityDetailForPErticularIsSuccess() {
+        let expectation = self.expectation(description: "success")
+        cityAddViewModel.fetchCityDetailWeatherForPerticularCity(cityId: 2147714) { (result) in
+            switch(result) {
+            case .success(let result):
+                XCTAssertNotNil(result)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    func testCitySearchTextResult() {
+        cityAddViewModel.searchCity(with: "Sydney") {
+            XCTAssertEqual(self.cityAddViewModel.cityInformation.count, 2)
+        }
+        
+        let searchCity = self.cityAddViewModel.cityInformation[0]
+        XCTAssertEqual(searchCity.id, 2147714)
+        XCTAssertEqual(searchCity.name, "Sydney")
+        XCTAssertEqual(searchCity.country, "AU")
+    }
+    
+    func testCitySearchTextResultIsNil() {
+        cityAddViewModel.searchCity(with: "pppp") {
+            XCTAssertEqual(self.cityAddViewModel.cityInformation.count, 0)
+        }
+    }
+    
+    override func tearDown() {
+        self.cityAddViewModel.cityOriginalArray = []
+        self.cityAddViewModel.cityInformation = []
+    }
+}
---
+++ WeatherInformation/WeatherInformationTests/CityDetailViewControllerTests.swift
@@ -0,0 +1,34 @@
+//
+//  CityDetailViewControllerTests.swift
+//  WeatherInformationTests
+//
+//  Created by Monika Jadhav on 21/12/20.
+//  Copyright © 2020 Monika Jadhav. All rights reserved.
+//
+
+import XCTest
+@testable import WeatherInformation
+
+class CityDetailViewControllerTests: XCTestCase {
+    
+    let cityDetailViewModel = CityDetailViewModel()
+    
+    override func setUp() {
+        
+    }
+    
+    func testFetchCityTemperatureDataIsSuccess() {
+        let expectation = self.expectation(description: "success")
+        cityDetailViewModel.fetchCityDetailWeatherInformation(cityId: 2147714) { (result) in
+            switch(result) {
+            case .success(let result):
+                XCTAssertNotNil(result)
+            case .failure(let error):
+                XCTFail(error.localizedDescription)
+            }
+            expectation.fulfill()
+        }
+        self.waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+}
